93f8872a541d35faeac9bdcc7f81b77d7b9e9073
Chloé Dequeker <chloe@dequeker.me>
2018-07-17 08:48:09 +0200

    Adding ranger config and updating muttrc

---
+++ .config/ranger/container/fsobject.py
@@ -0,0 +1,3 @@
+DOCUMENT_EXTENSIONS = ('cfg', 'css', 'cvs', 'djvu', 'doc', 'docx', 'gnm',
+                       'gnumeric', 'htm', 'html', 'md', 'odf', 'odg', 'odp', 'ods', 'odt', 
+                       'pod', 'ps', 'rtf', 'sxc', 'txt', 'xls', 'xlw', 'xml', 'xslx')
