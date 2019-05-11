fa38b44409ec0272748318df776595342f91f6f4
Chloé Dequeker <noreply@nelyah.eu>
2019-05-11 14:41:30 +0200

    Change nvim opening with Goyo for Astroid and only one initial inbox.

--- .fzf.bash
+++ .fzf.bash
@@ -1,7 +1,7 @@
 # Setup fzf
 # ---------
 if [[ ! "$PATH" == */home/chloe/.fzf/bin* ]]; then
-  export PATH="$PATH:/home/chloe/.fzf/bin"
+  export PATH="${PATH:+${PATH}:}/home/chloe/.fzf/bin"
 fi
 
 # Auto-completion
@@ -11,4 +11,3 @@ fi
 # Key bindings
 # ------------
 source "/home/chloe/.fzf/shell/key-bindings.bash"
-
