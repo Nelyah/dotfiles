1b9b221ab2687c08b95cdfb2cdf0ce8657c788b2
Chloé Dequeker <noreply@nelyah.eu>
2019-08-27 20:41:21 +0200

    alot: add hooks from the alot wiki and refine some bindings.

--- .config/alot/hooks.py
+++ .config/alot/hooks.py
@@ -1,3 +1,4 @@
+import alot
 import re
 
 
@@ -14,3 +15,19 @@ async def pre_envelope_send(ui, dbm, cmd):
         msg = 'no attachments. send anyway?'
         if not (await ui.choice(msg, select='yes')) == 'yes':
             raise Exception()
+
+
+def pre_buffer_open(ui, dbm, buf):
+    current = ui.current_buffer
+    if isinstance(current, alot.buffers.SearchBuffer):
+        current.focused_thread = current.get_selected_thread()   # remember focus
+
+
+def post_buffer_focus(ui, dbm, buf, success):
+    if success and hasattr(buf, "focused_thread"):  # if buffer has saved focus
+        if buf.focused_thread is not None:
+            tid = buf.focused_thread.get_thread_id() 
+            for pos, tlw in enumerate(buf.threadlist.get_lines()):
+                if tlw.get_thread().get_thread_id() == tid:
+                    buf.body.set_focus(pos)
+                    break
