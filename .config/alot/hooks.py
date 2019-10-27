6d44bb5ca75ac6b87ec622e29f84cd7c9c0776af
Chloé Dequeker <noreply@nelyah.eu>
2019-10-27 21:01:52 +0100

    alot: quote markers and better bindings

--- .config/alot/hooks.py
+++ .config/alot/hooks.py
@@ -10,7 +10,7 @@ def pre_buffer_focus(ui, dbm, buf):
 async def pre_envelope_send(ui, dbm, cmd):
     e = ui.current_buffer.envelope
     regex_attach = r'[Aa]ttach|[Pp]i[eè]ces?[\ -]?[Jj]ointes?|([Cc]i[\ -]?joint)'
-
+    
     if re.match(regex_attach, e.body, re.DOTALL) and not e.attachments:
         msg = 'no attachments. send anyway?'
         if not (await ui.choice(msg, select='yes')) == 'yes':
