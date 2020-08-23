263f9acb36a841f2f09e5a4992c42549d4e53f6d
Chloé Dequeker <noreply@nelyah.eu>
2020-08-23 18:49:31 +0200

    alot: update hook config

--- .config/alot/hooks.py
+++ .config/alot/hooks.py
@@ -2,16 +2,16 @@ import alot
 import re
 
 
-def pre_buffer_focus(ui, dbm, buf):
-	if buf.modename == 'search':
-		buf.rebuild()
+# def pre_buffer_focus(ui, dbm, buf):
+# 	if buf.modename == 'search':
+# 		buf.rebuild()
 
 
 async def pre_envelope_send(ui, dbm, cmd):
     e = ui.current_buffer.envelope
     regex_attach = r'[Aa]ttach|[Pp]i[eè]ces?[\ -]?[Jj]ointes?|([Cc]i[\ -]?joint)'
     
-    if re.match(regex_attach, e.body, re.DOTALL) and not e.attachments:
+    if re.match(regex_attach, e.body_txt, re.DOTALL) and not e.attachments:
         msg = 'no attachments. send anyway?'
         if not (await ui.choice(msg, select='yes')) == 'yes':
             raise Exception()
