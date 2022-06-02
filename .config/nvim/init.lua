3d6878e7c31ae442f69d1f12d70e2c3ff48b64e3
Nelyah <contact@nelyah.eu>
2022-06-02 09:13:51 +0200

    nvim(treesitter): Update config to install all parser
    This is following the deprecation of the "maintained" tag which could
    previously be used.

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -146,7 +146,7 @@ require('packer').startup(function(use)
         run = ':TSUpdate',
         config = function ()
             require'nvim-treesitter.configs'.setup({
-              ensure_installed = "maintained", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
+              ensure_installed = "all", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
               ignore_install = {}, -- List of parsers to ignore installing
               highlight = {
                 enable = true,              -- false will disable the whole extension
