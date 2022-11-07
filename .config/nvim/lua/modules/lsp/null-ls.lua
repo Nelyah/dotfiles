770fb95d17a7b46bd87bf5618996c39c44fe1699
Nelyah <contact@nelyah.eu>
2022-11-07 11:39:57 +0100

    nvim(formatter): Add golines formatter to null-ls configuration

--- .config/nvim/lua/modules/lsp/null-ls.lua
+++ .config/nvim/lua/modules/lsp/null-ls.lua
@@ -4,6 +4,7 @@ local null_ls = require("null-ls")
 
 M.sources = {
     null_ls.builtins.formatting.shfmt,
+    null_ls.builtins.formatting.golines,
     null_ls.builtins.formatting.stylua,
     null_ls.builtins.formatting.black.with({ extra_args = { "--line-length", "100" } }),
     null_ls.builtins.formatting.clang_format,
