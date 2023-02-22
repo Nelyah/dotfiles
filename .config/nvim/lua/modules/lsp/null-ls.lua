068e2cdd7ff50709884c4f22f2c825715538cf12
Nelyah <contact@nelyah.eu>
2023-02-22 15:42:16 +0100

    nvim(black-formatter): Remove default line-length configuration for black

--- .config/nvim/lua/modules/lsp/null-ls.lua
+++ .config/nvim/lua/modules/lsp/null-ls.lua
@@ -6,7 +6,7 @@ M.sources = {
     null_ls.builtins.formatting.shfmt,
     null_ls.builtins.formatting.golines,
     null_ls.builtins.formatting.stylua,
-    null_ls.builtins.formatting.black.with({ extra_args = { "--line-length", "100" } }),
+    null_ls.builtins.formatting.black,
     null_ls.builtins.formatting.clang_format,
 }
 
