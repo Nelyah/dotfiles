29349da7ba445b0cc0869ce36c48aae6241fd3af
Nelyah <contact@nelyah.eu>
2023-05-30 12:36:15 +0200

    nvim: Add prettier as a supported formatter

--- .config/nvim/lua/modules/lsp/null-ls.lua
+++ .config/nvim/lua/modules/lsp/null-ls.lua
@@ -6,6 +6,7 @@ M.sources = {
     null_ls.builtins.formatting.shfmt,
     null_ls.builtins.formatting.golines,
     null_ls.builtins.formatting.stylua,
+    null_ls.builtins.formatting.prettier,
     null_ls.builtins.formatting.black,
     null_ls.builtins.formatting.prettier,
     null_ls.builtins.formatting.clang_format,
