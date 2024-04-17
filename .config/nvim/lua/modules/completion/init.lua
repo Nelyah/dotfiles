22f658ede32e3e9cec14ba0b61683c3d8e2bb964
Nelyah <contact@nelyah.eu>
2024-04-17 13:37:04 +0200

    nvim: Add github copilot

--- .config/nvim/lua/modules/completion/init.lua
+++ .config/nvim/lua/modules/completion/init.lua
@@ -1,5 +1,16 @@
 local plugin = require("core.packer").register_plugin
 
+plugin({
+	"github/copilot.vim",
+	config = function()
+		vim.keymap.set("i", "<C-a>", 'copilot#Accept("\\<CR>")', {
+			expr = true,
+			replace_keycodes = false,
+		})
+		vim.g.copilot_no_tab_map = true
+		vim.g.copilot_enabled = true
+	end,
+})
 -- {{{ LuaSnip
 plugin({
 	"L3MON4D3/LuaSnip",
@@ -7,10 +18,10 @@ plugin({
 		require("luasnip.loaders.from_vscode").lazy_load()
 		vim.keymap.set("i", "<c-c>", function()
 			if require("luasnip").expand_or_jumpable() == true then
-                require("luasnip").expand_or_jump()
-                return
+				require("luasnip").expand_or_jump()
+				return
 			end
-            return "<c-c>"
+			return "<c-c>"
 		end)
 	end,
 	dependencies = { "rafamadriz/friendly-snippets", "nvim-cmp" },
