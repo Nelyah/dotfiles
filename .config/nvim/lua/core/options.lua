6f309fd33ba0a5364b065ecfe248237c0b9d5116
Nelyah <contact@nelyah.eu>
2024-05-22 13:54:02 +0200

    nvim: Add pretty diagnostic icons

--- .config/nvim/lua/core/options.lua
+++ .config/nvim/lua/core/options.lua
@@ -132,3 +132,9 @@ vim.opt.fillchars = {
     foldsep = "│",
     foldclose = "▸",
 }
+
+local signs = { Error = "󰅚 ", Warn = " ", Hint = "󰌶 ", Info = " " }
+for type, icon in pairs(signs) do
+  local hl = "DiagnosticSign" .. type
+  vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+end
