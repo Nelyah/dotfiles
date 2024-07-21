8acce413c4a34aecdf7207c5c3e74bc5721bf148
Nelyah <contact@nelyah.eu>
2024-07-21 13:31:57 +0200

    nvim: Put onedark configuration inline

--- .config/nvim/lua/modules/ui/init.lua
+++ .config/nvim/lua/modules/ui/init.lua
@@ -48,7 +48,11 @@ plugin({
 	"navarasu/onedark.nvim",
 	config = function()
 		require("modules.treesitter") -- needed for this theme
-		require("modules.ui.onedark").setup()
+		require("onedark").setup()
+		vim.cmd([[colorscheme onedark]])
+		vim.cmd([[highlight IncSearch guibg=#135564 guifg=white]])
+		vim.cmd([[highlight Search guibg=#135564 guifg=white]])
+		vim.cmd([[highlight Folded guibg=default guifg=grey]])
 	end,
 })
 -- }}}
