632792691371f8e66ac2ae9667aa03270b299a16
Nelyah <contact@nelyah.eu>
2025-04-11 11:56:14 +0200

    nvim: Make performance improvements for conflict markers

--- .config/nvim/lua/core/conflict_markers_highlight.lua
+++ .config/nvim/lua/core/conflict_markers_highlight.lua
@@ -1,3 +1,8 @@
+-- I don't want to execute this on files that are bigger than this
+-- Because we still read all lines and it could make start up WAY
+-- too slow (eg. file is multiple GB)
+local max_file_lines = 1000000
+
 local function setup()
 	local highlight_conflict_current_hi = "ConflictMarkerCurrentHi"
 	local highlight_conflict_current = "ConflictMarkerCurrent"
@@ -8,12 +13,16 @@ local function setup()
 
 	-- Define highlight groups for conflict markers
 	local white = "#dddddd"
-	vim.api.nvim_set_hl(0, highlight_conflict_current_hi, { fg = white, bg = "#522d2d", bold = true })
-	vim.api.nvim_set_hl(0, highlight_conflict_current, { bg = "#522d2d" })
-	vim.api.nvim_set_hl(0, highlight_conflict_parent_hi, { fg = white, bg = "#272727", bold = true })
-	vim.api.nvim_set_hl(0, highlight_conflict_parent, { bg = "#272727" })
-	vim.api.nvim_set_hl(0, highlight_conflict_incoming_hi, { fg = white, bg = "#353e32", bold = true })
-	vim.api.nvim_set_hl(0, highlight_conflict_incoming, { bg = "#353e32" })
+	local current_changes_bg = "#522d2d"
+	local parent_changes_bg = "#272727"
+	local incoming_changes_bg = "#353e32"
+
+	vim.api.nvim_set_hl(0, highlight_conflict_current_hi, { fg = white, bg = current_changes_bg, bold = true })
+	vim.api.nvim_set_hl(0, highlight_conflict_current, { bg = current_changes_bg })
+	vim.api.nvim_set_hl(0, highlight_conflict_parent_hi, { fg = white, bg = parent_changes_bg, bold = true })
+	vim.api.nvim_set_hl(0, highlight_conflict_parent, { bg = parent_changes_bg })
+	vim.api.nvim_set_hl(0, highlight_conflict_incoming_hi, { fg = white, bg = incoming_changes_bg, bold = true })
+	vim.api.nvim_set_hl(0, highlight_conflict_incoming, { bg = incoming_changes_bg })
 
 	-- Define markers as local variables so they can be reused
 	local current_change_marker        = "<<<<<<<"
@@ -34,12 +43,23 @@ local function setup()
 
 		-- Clear previous extmarks in the namespace
 		vim.api.nvim_buf_clear_namespace(bufnr, 0, 0, -1)
-		local lines = vim.api.nvim_buf_get_lines(bufnr, 0, -1, false)
-		local conflict_hl_name = nil
-		local has_started, has_ended = false, false
+		local lines                     = vim.api.nvim_buf_get_lines(bufnr, 0, -1, false)
+		local first_line                = vim.fn.line("w0")
+		local last_line                 = vim.fn.line("w$")
+		local conflict_hl_name          = nil
+		local has_started, has_ended    = false, false
+
+		local current_change_regex      = "^" .. current_change_marker
+		local marker_parent_regex       = "^" .. parent_change_marker
+		local marker_incoming_regex     = "^" .. start_incoming_change_marker
+		local marker_end_incoming_regex = "^" .. end_incoming_change_marker
 
 		for i, line in ipairs(lines) do
-			if line:match("^" .. current_change_marker) then
+			if i < first_line - 500 or i > last_line + 500 then
+				goto continue
+			end
+
+			if line:match(current_change_regex) then
 				if not has_ended then
 					-- If the previous conflict did not close properly, clear extmarks and use a new namespace.
 					vim.api.nvim_buf_clear_namespace(bufnr, conflict_ns, 0, -1)
@@ -48,11 +68,11 @@ local function setup()
 				end
 				conflict_hl_name = highlight_conflict_current_hi
 				has_started = true
-			elseif has_started and line:match("^" .. parent_change_marker) then
+			elseif has_started and line:match(marker_parent_regex) then
 				conflict_hl_name = highlight_conflict_parent_hi
-			elseif has_started and line:match("^" .. start_incoming_change_marker) then
+			elseif has_started and line:match(marker_incoming_regex) then
 				conflict_hl_name = highlight_conflict_incoming_hi
-			elseif has_started and line:match("^" .. end_incoming_change_marker) then
+			elseif has_started and line:match(marker_end_incoming_regex) then
 				conflict_hl_name = highlight_conflict_incoming_hi
 				has_ended = true
 			end
@@ -83,6 +103,8 @@ local function setup()
 					conflict_ns = vim.api.nvim_create_namespace("conflict_markers_" .. conflict_count)
 				end
 			end
+
+			::continue::
 		end
 
 		-- If the last conflict is not closed properly, clear its extmarks.
@@ -92,10 +114,10 @@ local function setup()
 	end
 	-- Automatically update conflict marker highlights on buffer read and text changes.
 	vim.api.nvim_create_augroup("NelyahGitConflictMarkers", { clear = true })
-	vim.api.nvim_create_autocmd({ "BufReadPost", "TextChanged", "TextChangedI" }, {
+	vim.api.nvim_create_autocmd({ "BufReadPost", "TextChanged", "InsertLeave" }, {
 		group = "NelyahGitConflictMarkers",
 		callback = function(args)
-			if vim.api.nvim_buf_line_count(args.buf) > 50000 then
+			if vim.api.nvim_buf_line_count(args.buf) > max_file_lines then
 				return
 			end
 			highlight_conflict_markers(args.buf)
@@ -109,9 +131,14 @@ vim.api.nvim_create_augroup("CustomHighlights", { clear = true })
 vim.api.nvim_create_autocmd("ColorScheme", {
 	group = "CustomHighlights",
 	callback = function()
+		if vim.api.nvim_buf_line_count(0) > max_file_lines then
+			return
+		end
 		setup()
 	end,
 })
 
 -- Load this module anyway
-setup()
+if vim.api.nvim_buf_line_count(0) < max_file_lines then
+	setup()
+end
