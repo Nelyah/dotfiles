18d769d05c7cca84d3b27e3de0a51e66ab04f35d
Nelyah <contact@nelyah.eu>
2025-10-02 09:52:57 +0200

    nvim: Add a spinner when I send a request to codecompanion

--- .config/nvim/lua/modules/completion/init.lua
+++ .config/nvim/lua/modules/completion/init.lua
@@ -22,13 +22,53 @@ plugin({
 		"nvim-treesitter/nvim-treesitter",
 	},
 	config = function()
+		local spinner = require("modules.completion.codecompanion.spinner")
+		spinner:init()
+
 		require("codecompanion").setup({
+			send = {
+				callback = function(chat)
+					vim.cmd("<esc>o")
+					vim.cmd("stopinsert")
+					chat:submit()
+					chat:add_buf_message({ role = "llm", content = "" })
+				end,
+				index = 1,
+				description = "Send",
+			},
 			strategies = {
+
 				chat = {
 					adapter = {
 						name = "copilot",
 						model = "gpt-5",
 					},
+					roles = {
+						llm = function(adapter)
+							local model_name = ''
+							if adapter.schema and adapter.schema.model and adapter.schema.model.default then
+								local model = adapter.schema.model.default
+								if type(model) == 'function' then
+									model = model(adapter)
+								end
+								model_name = ' (' .. model .. ')'
+							end
+							return '  ' .. adapter.formatted_name .. model_name
+						end,
+						user = ' User',
+					},
+				},
+				stop = {
+					modes = {
+						n = '<C-c>',
+					},
+					index = 4,
+					callback = 'keymaps.stop',
+					description = 'Stop Request',
+				},
+				agent = {
+					adapter = 'copilot',
+					model = "gpt-5",
 				},
 				inline = {
 					adapter = {
@@ -38,12 +78,8 @@ plugin({
 				},
 			},
 		})
-		vim.keymap.set("v", "<leader>ca", "<cmd>CodeCompanionChat Add<cr>", { noremap = true, silent = true })
-		vim.keymap.set(
-			{ "n", "v" },
-			"<leader>cc",
-			"<cmd>CodeCompanionChat Toggle<cr>",
-			{ noremap = true, silent = true }
+		vim.keymap.set("v", "<leader>cc", "<cmd>CodeCompanionChat Add<cr>", { noremap = true, silent = true })
+		vim.keymap.set("n", "<leader>cc", "<cmd>CodeCompanionChat Toggle<cr>", { noremap = true, silent = true }
 		)
 	end,
 })
